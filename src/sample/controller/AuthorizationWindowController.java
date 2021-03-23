94fafd483677cd0cc4f30fc9b3b43b15c180604a
DergachevDenis <dergache.di@gmail.com>
2021-03-23 20:33:52 +0300

    update AuthorizationWindow.fxml update AuthorizationWindowController.java add StyleApp.css

---
+++ src/sample/controller/AuthorizationWindowController.java
@@ -0,0 +1,28 @@
+package sample.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AuthorizationWindowController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField textFieldLogin;
+
+    @FXML
+    private PasswordField textFieldPassword;
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
